05f4d983e9c42fcd48eb0ccdd896e025ce41aac4
remotedeveloper88 <remotedeveloper88@gmail.com>
2016-09-27 18:10:15 -0300

    level 1 and 2 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
-    private Rigidbody rb;
     public int speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private static int levelNumber = 1;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
 	void FixedUpdate ()
@@ -26,6 +36,39 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            VerifyIfWon();
+        } else if (other.gameObject.CompareTag("Lava"))
+        {
+            RestartLevel();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void VerifyIfWon()
+    {
+        if (count >= 8)
+        {
+            //winText.text = "You Win!";
+            StartNewLevel();
+        }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene("level" + levelNumber.ToString());
+    }
+
+    void StartNewLevel()
+    {
+        levelNumber++;
+        count = 0;
+        SetCountText();
+        RestartLevel();
     }
 }
